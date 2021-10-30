3e4519b9ec3bbc549626ba365c48c39150250bf0
rouan <rouan.laporal@outlook.com>
2021-10-30 11:54:06 +0200

    function login functional, can get a user with is id

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,8 +1,12 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/Users');
 const jwt = require('jsonwebtoken');
-
-exports.signup = (req, res, next) => {
+/**
+ * Use to add a new user to the database
+ * @param {Object} req request send by the user with his username and his password in JSon
+ * @param {any} res response send to the user
+ */
+exports.signup = (req, res) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash =>{
         const user = new User({
@@ -14,4 +18,33 @@ exports.signup = (req, res, next) => {
         .catch( () => res.status(400).json({ error: 'l\'utilisateur n\'a pas pu être crée' }));
     })
     .catch(error => res.status(500).json({ error }));
+};
+/**
+ * Find a user in the database and compare the password
+ * @param {Object} req request send by the user with his username and his password in JSon
+ * @param {*} res 
+ */
+exports.login = (req,res)=>{
+    User.findOne({ username: req.body.username })
+    .then(user =>{
+        if(!user){
+            return res.status(401).json({ error: 'Utilisateur non trouvé' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid =>{
+            if(!valid){
+                return res.status(401).json({error: "Mot de passe incorrect" });
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch( error => res.status(500).json({ error }));
 };
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userCtrl = require('../Controllers/users');
 
 router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,5 +13,15 @@ router.get('/', function(req, res, next) {
 //Add a new user 
 router.post('/', async(req,res) =>{
   res.json(await mongoose.model('User').create(req.body));
+});
+
+//GET one user with is id
+router.get('/:id', async(req,res,next) =>{
+  res.json(await mongoose.model('User').findById(req.params.id, {password: 0 }));
+});
+
+//Update a user
+router.put('/:id', async(req,res)=> {
+  res.json(await mongoose.model('User').findByIdAndUpdate(req.params.id,req.body));
 });
 module.exports = router;
